a20d3f37cb19d2e74f36bf4122de82af62575ed7
IsPekToP <inspektop1@gmail.com>
2024-07-02 12:34:15 +0200

    остановился на 7.45

--- 15main5_1/itProger/blog/views.py
+++ 15main5_1/itProger/blog/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render
-#сначало будем выводить обычный текст:
-#для этого подключаем HttpResponse
-from django.http import HttpResponse
+#from django.http import HttpResponse
 
+
+# def home(request):
+#     return HttpResponse('<h3>Привет всем</h3>')
+
+# def contacti(request):
+#     return HttpResponse('<h3>Просто страница</h3>')
+
+
+#render - позволяет вывести HTML-шаблон
+#return render(request,'blog/home.html') - 1й пар-тр - тот же что и в home(request)
+#2й пар-тр - шаблон(пишем с учетом того что мы в папке templates - 'blog/home.html')
 
 def home(request):
-    return HttpResponse('<h3>Привет всем</h3>')
-
-# def about(request):
-#     return HttpResponse('<h3>Страница о нас</h3>')
+    return render(request,'blog/home.html')
 
 def contacti(request):
-    return HttpResponse('<h3>Просто страница</h3>')
+    return render(request,'blog/contacti.html')
--- 15main5_1/itProger/itProger/urls.py
+++ 15main5_1/itProger/itProger/urls.py
@@ -1,3 +1,25 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('blog.urls')),
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 """
 URL configuration for itProger project.
 
@@ -14,10 +36,3 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
-]
