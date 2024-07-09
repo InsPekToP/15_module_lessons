5eeaf4aac5156e675bbf3f1e6fc7a3096dc497cd
IsPekToP <inspektop1@gmail.com>
2024-07-09 12:18:23 +0200

    классы для табличек

--- 15main7_1/itProger/blog/models.py
+++ 15main7_1/itProger/blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class News(models.Model):
+    title = models.CharField('Название статьи',max_length=100)
+    text = models.TextField('Основной текст статьи')
+    date = models.DateTimeField(default=timezone.now)
+    avtor = models.ForeingKey(User, on_delete=models.CASCADE)
--- 15main7_1/itProger/blog/views.py
+++ 15main7_1/itProger/blog/views.py
@@ -1,32 +1,7 @@
 from django.shortcuts import render
 #from django.http import HttpResponse
 
-
-# def home(request):
-#     return HttpResponse('<h3>Привет всем</h3>')
-
-# def contacti(request):
-#     return HttpResponse('<h3>Просто страница</h3>')
-
-
-#render - позволяет вывести HTML-шаблон
-#return render(request,'blog/home.html') - 1й пар-тр - тот же что и в home(request)
-#2й пар-тр - шаблон(пишем с учетом того что мы в папке templates - 'blog/home.html')
-
-#чтобы работать с Jinja,надо в шаблоны передавать какую-либо инфу,а затем отображать в HTML
-
 def home(request):
-    #создаем словарь
-    # number = 50
-    # data = {
-    #     'some': number
-    # }
-    #теперь хотим передать эту инфу в blog/home.html
-    #для этого надо передать data 3м пар-тром
-
-    #усложним задачу
-    #создадим список,кот созтоит из словарей
-    #Инфа про статьи(далее будем это все через БД)
     news = [
         {
             'title' : 'Наша первая статья',
@@ -42,8 +17,7 @@ def home(request):
             # 'avtor' : 'Джон'
         }
     ]
-    #теперь вместо ключа some подставляем news 
-    #и подставляем значение список news
+
     data = {
         'news': news,
         #Сюда же подставляем 'title'
@@ -53,8 +27,5 @@ def home(request):
 
 
 def contacti(request):
-#сюда тоже можем передавать инфу,но другим способом
-#также можем создавать словари без пер-ой
-#и также теперь можем выводить это в contacti.html
     return render(request,'blog/contacti.html',{'title': 'Страница контакты1'})
 
